f0eb389ac4dfcc41043063bb60135afe1b6cf370
Andreas Tasoulas <andreas.tasoulas@gmail.com>
2021-03-06 13:58:31 +0200

    Display differences with previous day

--- main.py
+++ main.py
@@ -5,10 +5,16 @@ VACCINE_ENDPOINT = "https://opendata.digilugu.ee/opendata_covid19_vaccination_to
 HOSPITAL_ENDPOINT = "https://opendata.digilugu.ee/opendata_covid19_hospitalization_timeline.json"
 CURRENT_DATE = "2021-03-04"
 
+
 def get_json_data(url) -> any:
     # Open data endpoint
     r = requests.get(url=url)
     return r.json()
+
+
+def diff(current, previous, feature) -> any:
+    numdiff = int(current[feature]) - int(previous[feature])
+    return "+" + str(numdiff) if numdiff > 0 else str(numdiff)
 
 
 # json_data = get_json_data(API_ENDPOINT)
@@ -22,12 +28,22 @@ completedVaccinationPercentage = lastDayCompletedVaccinationData["PopulationCove
 partialVaccinationPercentage = round(lastDayVaccinationData["PopulationCoverage"] - lastDayCompletedVaccinationData["PopulationCoverage"], 3)
 
 currentDayHospital = json_hospital[-1]
+previousDayHospital = json_hospital[-2]
 
 print("\n")
 
 print("Completed vaccination percentage: " + str(completedVaccinationPercentage))
 print("Partial vaccination percentage: " + str(partialVaccinationPercentage) + "\n")
 
+diffVentilation = diff(currentDayHospital, previousDayHospital, "IsOnVentilation")
+diffIntensiveCare = diff(currentDayHospital, previousDayHospital, "IsInIntensive")
+diffNewCases = diff(currentDayHospital, previousDayHospital, "NewCases")
+
 print("On ventilation: " + str(currentDayHospital["IsOnVentilation"]))
+print("Difference: " + diffVentilation + "\n")
+
 print("In intensive care: " + str(currentDayHospital["IsInIntensive"]))
+print("Difference: " + diffIntensiveCare + "\n")
+
 print("New cases: " + str(currentDayHospital["NewCases"]))
+print("Difference: " + diffNewCases + "\n")
