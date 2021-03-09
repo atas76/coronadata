01689e0507b6f1ec69100ddffc6cb6c710a69952
Andreas Tasoulas <andreas.tasoulas@gmail.com>
2021-03-09 02:07:43 +0200

    Load country details in Country class

---
+++ country.py
@@ -0,0 +1,6 @@
+class Country:
+    def __init__(self, name, daily_cases, daily_deaths, population):
+        self.name = name
+        self.daily_cases = daily_cases
+        self.daily_deaths = daily_deaths
+        self.population = population
--- worldometer_main.py
+++ worldometer_main.py
@@ -1,23 +1,26 @@
 import json
+import country
+
+Country = country.Country
 
 
 def get_cases(country):
     try:
-        return int(country["daily cases"].replace(',', ''))
+        return int(country.daily_cases.replace(',', ''))
     except (ValueError, AttributeError):
         return 0
 
 
 def get_deaths(country):
     try:
-        return int(country["daily deaths"].replace(',', ''))
+        return int(country.daily_deaths.replace(',', ''))
     except (ValueError, AttributeError):
         return 0
 
 
 def get_population(country):
     try:
-        return int(country["population"].replace(',', ''))
+        return int(country.population.replace(',', ''))
     except (ValueError, AttributeError):
         return 0
 
@@ -26,14 +29,28 @@ def print_array(sorted_array):
     for entry in sorted_array:
         print(entry)
 
-
 with open('worldometer.jl') as f:
     country_data = json.load(f)
 
-sortedByPopulation = sorted(country_data, key=get_population, reverse=True)
-sortedByCases = sorted(country_data, key=get_cases, reverse=True)
-sortedByDeaths = sorted(country_data, key=get_deaths, reverse=True)
+countries = []
+for country_entry in country_data:
+    countries.append(Country(country_entry["country"], country_entry["daily cases"], country_entry["daily deaths"], country_entry["population"]))
 
-# print_array(sortedByPopulation)
-print_array(sortedByCases)
-# print_array(sortedByDeaths)
+sortedByPopulation = sorted(countries, key=get_population, reverse=True)
+sortedByCases = sorted(countries, key=get_cases, reverse=True)
+sortedByDeaths = sorted(countries, key=get_deaths, reverse=True)
+
+casesOrdinal = 1
+
+# for country in sortedByDeaths:
+#    print(str(casesOrdinal) + ", " + country.name + ", " + str(country.daily_deaths))
+#    casesOrdinal = casesOrdinal + 1
+
+for country in sortedByCases:
+    print(str(casesOrdinal) + ", " + country.name + ", " + str(country.daily_cases))
+    casesOrdinal = casesOrdinal + 1
+
+# for country in sortedByPopulation:
+#    print(str(casesOrdinal) + ", " + country.name + ", " + country.population)
+#    casesOrdinal = casesOrdinal + 1
+
