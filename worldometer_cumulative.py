11bfbe69aec7d26735aa7f99a058eae78173657f
Andreas Tasoulas <andreas.tasoulas@gmail.com>
2021-03-13 20:00:16 +0200

    Cumulative statistics: filter files by category

---
+++ worldometer_cumulative.py
@@ -0,0 +1,8 @@
+from os import walk
+from re import search
+
+_, _, filenames = next(walk('./reports'))
+
+casesFiles = filter(lambda filename: search('daily_cases_all', filename), filenames)
+deathFiles = filter(lambda filename: search('daily_deaths_all', filename), filenames)
+testsFiles = filter(lambda filename: search('tests', filename), filenames)
