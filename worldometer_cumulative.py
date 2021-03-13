57cce4ef9e172489c90b291ce0e9701bf0ffae0e
Andreas Tasoulas <andreas.tasoulas@gmail.com>
2021-03-13 20:02:29 +0200

    Minor

--- worldometer_cumulative.py
+++ worldometer_cumulative.py
@@ -4,5 +4,5 @@ from re import search
 _, _, filenames = next(walk('./reports'))
 
 casesFiles = filter(lambda filename: search('daily_cases_all', filename), filenames)
-deathFiles = filter(lambda filename: search('daily_deaths_all', filename), filenames)
+deathsFiles = filter(lambda filename: search('daily_deaths_all', filename), filenames)
 testsFiles = filter(lambda filename: search('tests', filename), filenames)
