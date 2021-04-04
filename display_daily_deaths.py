cd52f610040778808409e7950cb4b8860ae9f062
Andreas Tasoulas <andreas.tasoulas@gmail.com>
2021-04-04 19:32:24 +0300

    Convert to html

--- display_daily_deaths.py
+++ display_daily_deaths.py
@@ -4,4 +4,4 @@ from datetime import date, timedelta
 today = date.today() - timedelta(1)
 current_date = today.strftime("%Y%m%d")[2:]
 
-os.system("csvtotable ./reports/report_" + "daily_cases_" + current_date + ".csv --serve -c DailyCases")
+os.system("csvtotable ./reports/report_" + "daily_deaths_" + current_date + ".csv --serve -c DailyDeaths")
